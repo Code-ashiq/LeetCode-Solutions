0b7dffe8ee0e8c7ef12a866e838480f1b984804f
Code-ashiq <ashiqmohamed886@gmail.com>
2025-06-02 21:45:12 +0530

    add: new solutions

--- LeetCode_Solutions.py
+++ LeetCode_Solutions.py
@@ -66,3 +66,15 @@ class Solution:
 sol = Solution()
 print(sol.findClosestNumber([-4,-3,1,4,6]))
 
+
+#problem 1.Two Sum, Level : Easy
+
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        for i in range(0, len(nums)): # iterate over each element by index i
+            for j in range(i+1, len(nums)): # iterate over elements after i
+                if nums[i] + nums[j] == target: # check if sum equals target
+                    return i,j #return the pair of indices
+
+sol = Solution()
+print(sol.twoSum([1,3,5,4],5))
