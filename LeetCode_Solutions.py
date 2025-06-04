447fdda1a695c5c505c553a58d13f7d24890559d
Code-ashiq <ashiqmohamed886@gmail.com>
2025-06-04 13:02:07 +0530

    minor changes

--- LeetCode_Solutions.py
+++ LeetCode_Solutions.py
@@ -84,19 +84,23 @@ print(sol.twoSum([1,3,5,4],5))
 
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        result = []
-        i,j = 0,0
+        result = [] #list to store result
+        i,j = 0,0 # initialize two pointers
         
-        while i < len(word1) or j < len(word2):
+        #loop till both pointers reach the end
+        while i < len(word1) or j < len(word2): 
 
+            # if characters left in word1, append to result and move pointer
             if i < len(word1):
                 result.append(word1[i])
                 i += 1
 
+            # if characters left in word2, append to result and move pointer
             if j < len(word2):
                 result.append(word2[j])
                 j += 1
 
+        # convert list of characters into string using join() 
         return "".join(result)
 
 sol = Solution()
