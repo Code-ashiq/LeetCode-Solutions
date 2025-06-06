b4cffde6c09f797a004d90cafc80e1b43a0b6786
Code-ashiq <ashiqmohamed886@gmail.com>
2025-06-06 20:59:23 +0530

    add: comments

--- LeetCode_Solutions.py
+++ LeetCode_Solutions.py
@@ -136,15 +136,20 @@ print(sol.isSubsequence("ace", "abcde"))
 
 class Solution:
     def maxProfit(self, prices: list[int]) -> int:
-        l, r = 0, 1  #left = buy, right = sell
-        max_profit = 0
+        l, r = 0, 1  # initialize two pointers: l is buy, r is sell
+        max_profit = 0 # variable to store the maximum profit found
+        
+        # Loop until r pointer reaches the end of the list
         while r < len(prices):
+
             if prices[l] < prices[r]:
-                profit = prices[r] - prices[l]
-                max_profit = max(max_profit, profit)
+                profit = prices[r] - prices[l] # calculate the profit
+                max_profit = max(max_profit, profit) # update max_profit if the profit is higher
             else:
+                # if the price at r is less than or equal to price at l
+                # it means a better buying opportunity, so move l to r
                 l = r
-            r += 1
+            r += 1 # always move the right pointer forward
         return max_profit
     
 sol = Solution()
