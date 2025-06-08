48954a5a15d1e8a43660879616cc768715920d95
Code-ashiq <ashiqmohamed886@gmail.com>
2025-06-08 20:45:15 +0530

    add: new solution

--- LeetCode_Solutions.py
+++ LeetCode_Solutions.py
@@ -155,3 +155,21 @@ class Solution:
 sol = Solution()
 print(sol.maxProfit([7,1,5,3,6,4]))
 
+
+# problem 14. Longest Common Prefix, Level : Easy
+
+class Solution:
+    def longestCommonPrefix(self, strs: list[str]) -> str:
+        ans = ""
+        strs = sorted(strs)
+        first = strs[0]
+        last = strs[-1]
+        for i in range(min(len(first), len(last))):
+            if(first[i] != last[i]):
+                return ans
+            ans += first[i]
+        return ans
+
+sol = Solution()
+print(sol.longestCommonPrefix(["flower","flow","flight"]))
+
