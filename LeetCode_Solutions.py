6d082e099fbbcab295d87bda4937f1d0847321d7
Code-ashiq <ashiqmohamed886@gmail.com>
2025-06-08 21:29:53 +0530

    add: comments

--- LeetCode_Solutions.py
+++ LeetCode_Solutions.py
@@ -160,14 +160,25 @@ print(sol.maxProfit([7,1,5,3,6,4]))
 
 class Solution:
     def longestCommonPrefix(self, strs: list[str]) -> str:
-        ans = ""
+        ans = ""  # empty string to store the common prefix
+        
+        # Sort the list of strings lexicographically(that means dictionary order)
+        # after sorting, the first and last strings will be the most different
         strs = sorted(strs)
+
+        # Take the first and last strings in the sorted list
         first = strs[0]
         last = strs[-1]
+
+        # compare characters of the first and last strings
         for i in range(min(len(first), len(last))):
+            # if characters don't match, return the prefix found so far
             if(first[i] != last[i]):
                 return ans
+            # if characters match, add to the answer
             ans += first[i]
+
+        # if loop completes, return the full common prefix
         return ans
 
 sol = Solution()
