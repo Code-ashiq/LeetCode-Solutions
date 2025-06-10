a87398185631216553212c4a6687b0d9f498d73f
Code-ashiq <ashiqmohamed886@gmail.com>
2025-06-10 20:01:08 +0530

    add: new solution

--- LeetCode_Solutions.py
+++ LeetCode_Solutions.py
@@ -184,3 +184,28 @@ class Solution:
 sol = Solution()
 print(sol.longestCommonPrefix(["flower","flow","flight"]))
 
+
+# problem 228. Summary Ranges, Level : Easy
+
+class Solution:
+    def summaryRanges(self, nums: list[int]) -> list[str]:
+        ans = []
+        i = 0
+
+        while i < len(nums):
+            start = nums[i]
+
+            while i < len(nums)-1 and nums[i] + 1 == nums[i+1]:
+                i += 1
+            if start != nums[i]:
+                ans.append(str(start) + '->' + str(nums[i]))
+            else:
+                ans.append(str(nums[i]))
+
+            i += 1
+
+        return ans
+
+sol = Solution()
+print(sol.summaryRanges([0,1,2,4,5,7]))
+print(sol.summaryRanges([0,2,3,4,6,8,9]))
