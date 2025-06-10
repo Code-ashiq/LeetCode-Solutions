a24dcf98e3f9c3dd366e26f4676ef00389ca6414
Code-ashiq <ashiqmohamed886@gmail.com>
2025-06-10 20:18:22 +0530

    add: comments

--- LeetCode_Solutions.py
+++ LeetCode_Solutions.py
@@ -190,19 +190,22 @@ print(sol.longestCommonPrefix(["flower","flow","flight"]))
 class Solution:
     def summaryRanges(self, nums: list[int]) -> list[str]:
         ans = []
-        i = 0
+        i = 0 # Initialize pointer to traverse the nums list
 
         while i < len(nums):
-            start = nums[i]
+            start = nums[i] # Mark the start of a potential range
 
+            # Continue moving pointer while numbers are consecutive
             while i < len(nums)-1 and nums[i] + 1 == nums[i+1]:
                 i += 1
-            if start != nums[i]:
-                ans.append(str(start) + '->' + str(nums[i]))
-            else:
-                ans.append(str(nums[i]))
 
-            i += 1
+            # Check if we found a range (more than one number)
+            if start != nums[i]:
+                ans.append(str(start) + '->' + str(nums[i])) # Add range to answer
+            else:
+                ans.append(str(nums[i])) # Add single number to number
+
+            i += 1 # Move to next number to start new range check
 
         return ans
 
