070e903ccd2fb5a90eeab1ca7716c171c740bdd9
Code-ashiq <ashiqmohamed886@gmail.com>
2025-07-02 20:43:56 +0530

    add: comments

--- HashMap/contains_duplicate.py
+++ HashMap/contains_duplicate.py
@@ -2,12 +2,16 @@
 
 class Solution:
     def containsDuplicate(self, nums: list[int]) -> bool:
+        # Initialize an empty set to store unique elements
         s = set()
         for num in nums:
             if num in s:
-                return True
+                return True # Duplicate found, return True immediately
             else:
+                # If not in the set, add the number to the set
                 s.add(num)
+        
+        # If the loop completes with no duplicates, return False
         return False
     
 sol = Solution()
