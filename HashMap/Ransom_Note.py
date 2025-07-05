43ae5f0ddf3d5403e4d2eba1c2aa3f3bb6feb971
Code-ashiq <ashiqmohamed886@gmail.com>
2025-07-05 20:18:49 +0530

    add: comments

--- HashMap/Ransom_Note.py
+++ HashMap/Ransom_Note.py
@@ -2,13 +2,18 @@ from collections import Counter
 
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        # Count the frequency of each character in the magazine
         hashmap = Counter(magazine)
 
+        # Iterate over each character in the ransom note
         for char in ransomNote:
+            # If the character is available in the magazine (count > 0), use one occurrence
             if hashmap[char] > 0:
                 hashmap[char] -= 1
             else:
+                # If the character is not available, ransom note can't be constructed
                 return False
+        # All characters are available; ransom note can be constructed
         return True
     
 sol = Solution()
