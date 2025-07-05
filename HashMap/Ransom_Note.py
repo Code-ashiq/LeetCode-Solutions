083ef3d865efdf5937ae8f5e86b9cf32b9193dff
Code-ashiq <ashiqmohamed886@gmail.com>
2025-07-05 20:14:08 +0530

    add: new solution

---
+++ HashMap/Ransom_Note.py
@@ -0,0 +1,15 @@
+from collections import Counter
+
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        hashmap = Counter(magazine)
+
+        for char in ransomNote:
+            if hashmap[char] > 0:
+                hashmap[char] -= 1
+            else:
+                return False
+        return True
+    
+sol = Solution()
+print(sol.canConstruct("aa", "aab"))
