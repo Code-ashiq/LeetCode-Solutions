2e051e18517451e747749394c505e9120b1357fe
Code-ashiq <ashiqmohamed886@gmail.com>
2025-07-06 14:11:44 +0530

    add: new solutions

---
+++ HashMap/valid_anagram.py
@@ -0,0 +1,13 @@
+#problem 242.Valid Anagram, Level : Easy
+
+from collections import Counter
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        s_dict = Counter(s)
+        t_dict = Counter(t)
+
+        return s_dict == t_dict
