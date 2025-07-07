8dbd2fd716ba0be903165f802a160d51b9e55836
Code-ashiq <ashiqmohamed886@gmail.com>
2025-07-07 17:07:12 +0530

    add: comments

--- HashMap/valid_anagram.py
+++ HashMap/valid_anagram.py
@@ -3,11 +3,17 @@
 from collections import Counter
 
 class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
+    def isAnagram(s: str, t: str) -> bool:
+        # If lengths differ, they can't be anagrams
         if len(s) != len(t):
             return False
 
+         # Count frequency of each character in both strings
         s_dict = Counter(s)
         t_dict = Counter(t)
 
+         # Return True if both dictionaries are equal (i.e., same characters with same frequency)
         return s_dict == t_dict
+
+sol = Solution()
+print(Solution.isAnagram("anagram", "nagaram"))
