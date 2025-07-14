fa0cad83ae87f4568fb63bdf07e7f2985d89c6f7
Code-ashiq <ashiqmohamed886@gmail.com>
2025-07-14 20:54:02 +0530

    add: comments

--- TwoPointers/squares_of_a_sorted_array.py
+++ TwoPointers/squares_of_a_sorted_array.py
@@ -1,19 +1,24 @@
-# problem 977. Squares of a Sorted Array
+# problem 977. Squares of a Sorted Array, Level: Easy
 
 class Solution:
     def sortedSquares(self, nums: list[int]) -> list[int]:
+         # Initialize two pointers
         left= 0
         right = len(nums) -1
         result = []
 
+        # Traverse the list from both ends
         while left <= right:
+            # Compare the absolute values at both ends
             if abs(nums[left]) > abs(nums[right]):
+                # Square the larger absolute value and add to result
                 result.append(nums[left] **2)
-                left += 1
+                left += 1  # Move left pointer forward
             else:
                 result.append(nums[right] **2)
-                right -= 1
-            
+                right -= 1  # Move right pointer forward
+
+        # The result is in reverse order, so reverse it    
         result.reverse()
 
         return result
