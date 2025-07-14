12b53d993c6fdfe2000b8ce7e455b6cdee917b90
Code-ashiq <ashiqmohamed886@gmail.com>
2025-07-14 21:52:53 +0530

    add: comments

--- TwoPointers/reverse_string.py
+++ TwoPointers/reverse_string.py
@@ -2,11 +2,16 @@
 
 class Solution:
     def reverseString(self, s: list[str]) -> str:
+         # Initialize two pointers
         left = 0
         right = len(s) -1
 
+        # Loop until the two pointers meet or cross
         while left < right:
+            # Swap the characters at left and right positions
             s[left],s[right] = s[right],s[left]
+            
+            # Move the left pointer forward and the right pointer backward
             left += 1
             right -= 1
 
