111f86f47e543bbc55d12956b0353214b5b76117
Code-ashiq <ashiqmohamed886@gmail.com>
2025-07-21 20:54:21 +0530

    add: comments

--- Stacks & Queues/baseball_game.py
+++ Stacks & Queues/baseball_game.py
@@ -1,18 +1,22 @@
 class Solution:
     def calPoints(self, operations: list[str]) -> int:
-        stk = []
+        stk = [] # Stack to keep track of valid scores
 
         for op in operations:
             if op == "+":
+                 # "+" means sum of last two valid scores
                 stk.append(stk[-1] + stk[-2])
             elif op == "D":
+                # "D" means double the last valid score
                 stk.append(stk[-1] * 2)
             elif op == "C":
+                # "C" means remove the last valid score
                 stk.pop()
             else:
+                # Otherwise, it's a valid integer score
                 stk.append(int(op))
 
-        return sum(stk)
+        return sum(stk)  # Return total score
 
 sol = Solution()
 print(sol.calPoints(["5","2","C","D","+"]))
