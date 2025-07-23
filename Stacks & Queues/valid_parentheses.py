4c6871c43c7395ebb3956376c9b99ff25e9a85fc
Code-ashiq <ashiqmohamed886@gmail.com>
2025-07-23 23:00:28 +0530

    add: comments

--- Stacks & Queues/valid_parentheses.py
+++ Stacks & Queues/valid_parentheses.py
@@ -2,17 +2,31 @@
 
 class Solution:
     def isValid(self, s: str) -> bool:
+         # Mapping of closing brackets to their corresponding opening brackets
         hashmap = {')': '(', ']': '[', '}': '{'}
-        stk = []
+        stk = [] # Stack to keep track of opening brackets
 
         for c in s:
             if c not in hashmap:
+                # If character is an opening bracket, push to stack
                 stk.append(c)
             else:
+                # If stack is empty and we encounter a closing bracket, it's invalid
                 if not stk:
                     return False
                 else:
+                    # Pop the last opening bracket from the stack
                     popped = stk.pop()
+                    # If it doesn't match the expected opening bracket, it's invalid
                     if popped != hashmap[c]:
                         return False
+                    
+        # If stack is empty at the end, all brackets are matched correctly
         return not stk
+
+sol = Solution()
+print(sol.isValid("()"))
+print(sol.isValid("()[]{}"))
+print(sol.isValid("(]"))
+print(sol.isValid("([])"))
+print(sol.isValid("([)]"))
